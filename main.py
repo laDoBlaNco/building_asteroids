0a0ff115518e04d371368f29f21ad19d265d12e3
ladoblanco <whitesidekevin@gmail.com>
2024-12-17 22:46:03 -0400

    Bootdev Building Asteroids project update 2_2

--- main.py
+++ main.py
@@ -2,9 +2,21 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
-  print("Starting asteroids!")
+  pygame.init()
+  screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+  surface = pygame.Surface((SCREEN_WIDTH,SCREEN_HEIGHT))
+  
+  while True:
+    for event in pygame.event.get():
+      if event.type==pygame.QUIT:
+        return
+    surface.fill((0,0,0))
+    pygame.display.flip()
+
+
 
 
 if __name__ == '__main__':
