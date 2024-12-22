1352d0e20fd2ad5bf714001cec99f84ab85aad21
ladoblanco <whitesidekevin@gmail.com>
2024-12-21 23:55:03 -0400

    Bootdev building asteroids project 12/21 update

--- main.py
+++ main.py
@@ -3,18 +3,26 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
+
 
 def main():
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-  surface = pygame.Surface((SCREEN_WIDTH,SCREEN_HEIGHT))
+  clock = pygame.time.Clock()
+  player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+  dt = 0
   
   while True:
     for event in pygame.event.get():
       if event.type==pygame.QUIT:
         return
-    surface.fill((0,0,0))
+    screen.fill("black")
+    player.update(dt)
+    player.draw(screen)
     pygame.display.flip()
+    
+    dt = clock.tick(60)/1000
 
 
 
